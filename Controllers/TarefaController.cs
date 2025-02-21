ce067aeda1e4fd6b44c5d202aec54a2f122a054c
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-02-21 11:40:28 -0300

    added - Trilha Net API Desafio - [GET] Criando o endpoint ObterPorStatus

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -57,8 +57,9 @@ namespace TrilhaApiDesafio.Controllers
         public IActionResult ObterPorStatus(EnumStatusTarefa status)
         {
             // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
             var tarefa = _context.Tarefas.Where(x => x.Status == status);
+            
+            // Dica: Usar como exemplo o endpoint ObterPorData
             return Ok(tarefa);
         }
 
